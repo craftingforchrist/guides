522c805b9512d0f9623138e98407d12d0392e8f6
Ben Robson <ben@reliableit.net.au>
2024-06-06 12:38:59 +1000

    Base URL and fix deploy.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://craftingforchrist.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/guides/',
+  baseUrl: '/guides/', // Base url for guides
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
