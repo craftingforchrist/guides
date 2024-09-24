594d53d49b98893439ce6dfe4bbcc7237dbb66fd
Ben Robson <shadowolfbusiness@gmail.com>
2024-09-24 13:53:25 +1000

    Change baseUrl.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://craftingforchrist.github.io",
+  url: "https://guides.craftingforchrist.net",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/guides/", // Base url for guides
+  baseUrl: "/", // Base url for guides
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
