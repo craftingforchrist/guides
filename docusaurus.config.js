47db15b49286d7706a424807b709cc25988640d7
Ben Robson <shadowolfbusiness@gmail.com>
2025-03-04 12:01:07 +1100

    Move base routing path.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/", // Base url for guides
+  routeBasePath: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -81,7 +82,7 @@ const config = {
             label: "Guides",
           },
           {
-            href: "/docs/safeserver",
+            href: "/safeserver",
             label: "Safe Server",
             position: "left",
           },
