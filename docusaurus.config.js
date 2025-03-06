3b669a83a0beb45ecfa640df966c340c8a75081a
benrobson <shadowolfbusiness@gmail.com>
2025-03-06 22:14:46 +1100

    Remove the react front page and have introduction be front, update deps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,41 +1,22 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Guides",
+  title: "Knowledgebase",
   tagline: "The site for community questions, answer and guides.",
   favicon: "img/favicon.ico",
-
-  // Set the production url of your site here
-  url: "https://guides.craftingforchrist.net",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/", // Base url for guides
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "craftingforchrist", // Usually your GitHub org/user name.
-  projectName: "guides", // Usually your repo name.
+  url: "https://kb.craftingforchrist.net",
+  baseUrl: "/",
+  organizationName: "craftingforchrist",
+  projectName: "guides",
   deploymentBranch: "gh-pages",
   trailingSlash: false,
-
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
   },
-
   presets: [
     [
       "classic",
@@ -43,15 +24,12 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/craftingforchrist/guides/tree/master/packages/create-docusaurus/templates/shared/",
+          routeBasePath: "/",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/craftingforchrist/guides/tree/master/packages/create-docusaurus/templates/shared/",
         },
@@ -61,14 +39,12 @@ const config = {
       }),
     ],
   ],
-
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "Crafting For Christ Guides",
+        title: "Crafting For Christ Knowledgebase",
         logo: {
           alt: "Crafting For Christ Logo",
           src: "img/logo.svg",
@@ -76,9 +52,9 @@ const config = {
         items: [
           {
             type: "docSidebar",
-            sidebarId: "guidesSidebar",
+            sidebarId: "kbSidebar",
             position: "left",
-            label: "Guides",
+            label: "Knowledgebase",
           },
           {
             href: "/safeserver",
@@ -92,7 +68,7 @@ const config = {
           },
           {
             href: "https://github.com/craftingforchrist/guides",
-            label: "Guides Source",
+            label: "KB Source",
             position: "right",
           },
         ],
@@ -119,7 +95,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Crafting For Christ - Guides. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Crafting For Christ - Knowledgebase. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
